2a7527bcbe0244da602641109c817ea367c84071
Hugo Tarin Gonzalez <taringonzalezhugo@gmail.com>
2025-02-21 12:27:59 +0100

    Update Client
    Delete second client in the same Class

--- ChatLAN/src/Client/Client.java
+++ ChatLAN/src/Client/Client.java
@@ -7,7 +7,6 @@ import java.net.Socket;
 public class Client {
     Socket socket = null;
     BufferedReader in = null;
-    BufferedReader inExtern = null;
     DataOutputStream out = null;
 
     public Client(String ip, int port) throws IOException {
@@ -15,7 +14,6 @@ public class Client {
             socket = new Socket(InetAddress.getByName(ip), port);
             in = new BufferedReader(new InputStreamReader(System.in));
             out = new DataOutputStream(socket.getOutputStream());
-            // inExtern = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Recibe un mensaje del server, como mensajes de otros clientes
             String mess = "";
             mess = in.readLine();
             out.writeUTF(mess);
@@ -32,7 +30,5 @@ public class Client {
     }
     public static void main(String[] args) throws IOException {
         Client client = new Client("0.0.0.0",5000);
-
-        Client client1 = new Client("0.0.0.0",5000);
     }
 }
